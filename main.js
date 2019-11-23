28bd6c3f2f50727a78196a423b772fff359a1460
Adam <adam.castles@gmail.com>
2019-11-23 12:42:48 -0500

    Revert back to original r2d2 class

--- main.js
+++ main.js
@@ -6,11 +6,10 @@ async function main() {
     const r2d2 = await connect_function.connect(Scanner, R2D2)
     if (r2d2){
         // await r2d2.configureSensorStream();
-        // await r2d2.playAnimation(3);
+        await r2d2.setStance(Stance.tripod);
         await Utils.wait(2000);
-        await r2d2.setStance(Stance.bipod);
-        await Utils.wait(2000);
-        // await Utils.wait(2000);
+        await r2d2.playAnimation(20);
+        await r2d2.turnDome(20)
         await r2d2.playAudioFile(2);
         await Utils.wait(10000);
         await r2d2.setStance(Stance.tripod);
