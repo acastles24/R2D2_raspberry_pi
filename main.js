ad36f8492cb8f3027f7ca0f65ab879fbdcfeef22
Adam <adam.castles@gmail.com>
2019-11-23 13:58:03 -0500

    Created initialization function to store commands

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
 const { Scanner, Stance, Utils, R2D2 } = require('spherov2.js');
-let connect_function = require('./utilities/connect')
-const WAIT_TIME = 7000;
+const startup = require('./r2d2_functions/startup')
+const functions = require('./r2d2_functions/functions')
+const connect_function = require('./utilities/connect')
 
 async function main() {
     const r2d2 = await connect_function.connect(Scanner, R2D2)
     if (r2d2){
-        // await r2d2.configureSensorStream();
-        await r2d2.setStance(Stance.tripod);
-        await Utils.wait(2000);
-        await r2d2.playAnimation(20);
-        await r2d2.turnDome(20)
-        await r2d2.playAudioFile(2);
-        await Utils.wait(10000);
-        await r2d2.setStance(Stance.tripod);
-    }
-    else{
-        console.log('not found')
+        const r2d2_initialized = new functions.r2d2Initialize(r2d2, Scanner, Utils, Stance)
+        await r2d2_initialized.start_r2d2()
+        await r2d2_initialized.play_animation(3)
+        await r2d2_initialized.turn_dome_with_wait(45)
+        await r2d2_initialized.set_stance(3);
+        await r2d2_initialized.set_stance(2);
     }
 }
 
---
+++ r2d2_functions/functions.js
@@ -0,0 +1,38 @@
+class r2d2Initialize{
+    constructor(r2d2_found, scanner, utils, stance){
+        this.r2d2_found = r2d2_found
+        this.scanner = scanner
+        this.utils = utils
+        this.stance = stance
+}
+
+async turn_dome_with_wait(degrees) {
+    await this.r2d2_found.turnDome(degrees);
+    await this.utils.wait(2000);
+}
+
+async play_animation(anim_num){
+    await this.r2d2_found.playAnimation(anim_num);
+    await this.utils.wait(10000);
+}
+
+async start_r2d2(){
+    await this.set_stance(3);
+    await this.utils.wait(5000);
+}
+async set_stance(num_legs){
+    if (num_legs === 2) {
+        await this.r2d2_found.setStance(this.stance.bipod)
+    } else if (num_legs === 3) {
+        await this.r2d2_found.setStance(this.stance.tripod)
+    } else {
+        return
+    }
+    await this.utils.wait(2000);
+    
+}
+}
+
+
+
+exports.r2d2Initialize = r2d2Initialize
