f0b9d61f8b365c19c921a24d567597037dd1d3e4
Adam <adam.castles@gmail.com>
2019-11-23 12:22:05 -0500

    Copied new functions to R2D2 class

--- main.js
+++ main.js
@@ -5,12 +5,18 @@ const WAIT_TIME = 7000;
 async function main() {
     const r2d2 = await connect_function.connect(Scanner, R2D2)
     if (r2d2){
-        await r2d2.playAnimation(3);
-        await Utils.wait(WAIT_TIME);
-        await r2d2.turnDome(180);
-        await Utils.wait(3000);
+        // await r2d2.configureSensorStream();
+        // await r2d2.playAnimation(3);
+        await Utils.wait(2000);
+        await r2d2.setStance(Stance.bipod);
+        await Utils.wait(2000);
+        // await Utils.wait(2000);
+        await r2d2.playAudioFile(2);
+        await Utils.wait(10000);
         await r2d2.setStance(Stance.tripod);
-        return 
+    }
+    else{
+        console.log('not found')
     }
 }
 
--- utilities/connect.js
+++ utilities/connect.js
@@ -1,6 +1,4 @@
-async function connect(scanner, robot){
+exports.connect = async(scanner, robot) => {
   const r2d2_connected = await scanner.find(robot.advertisement);
   return r2d2_connected
 }
-
-module.exports.connect = connect;
