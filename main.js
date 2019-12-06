4eaa8a7c9b9d3f96e12447d473f5a348a6c63f2b
Adam <adam.castles@gmail.com>
2019-12-05 20:27:21 -0500

    Added subscribe and unsubscribe logic, made drivemethod subscribe

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const { Scanner, Stance, Utils, R2D2 } = require('spherov2.js');
-const functions = require('./r2d2_functions/functions')
-const mqtt = require('./utilities/mqtt_connection')
+const {r2d2Functions} = require('./r2d2_functions/move_functions')
+const {MQTTConnection} = require('./utilities/mqtt_connection')
 
 class CreateRobot{
     constructor(){
-        r2d2_connected = this.connect(Scanner, R2D2);
-        this.r2d2_functions = new functions.r2d2Functions(r2d2_connected, Utils, Stance)
-        this.client = new mqtt.MQTTConnection('192.168.1.13', r2d2_functions)
-
+        r2d2_connected = connect(Scanner, R2D2);
+        this.r2d2_functions = new r2d2Functions(r2d2_connected, Utils, Stance)
+        this.client = new MQTTConnection('192.168.1.13', r2d2_functions)
+}
 }
 
-subscribeDriveMethod(topic) {
+function subscribeDriveMethod(topic) {
     this.client.subscribe(topic)
 }
 
-unsubscribeDriveMethod(topic){
+function unsubscribeDriveMethod(topic){
     this.client.unsubscribe(topic)
 }
 
@@ -26,25 +26,26 @@ unsubscribeDriveMethod(topic){
  * @returns {Connected R2-D2 instance}
  * todo: retry if connection failed
  */
-async connect(scanner, R2D2) {
+async function connect(scanner, R2D2) {
     const r2d2_connected = await scanner.find(R2D2.advertisement);
     return r2d2_connected
-  }
 }
 
 
-main() {
+function main() {
     r2d2 = CreateRobot()
     // todo: static ip
     // todo: without wifi?
     // todo: incorporate into app
-    current_drive_method = 'manual'
-    drive_method_received = 'bleh'
+    current_drive_method = null
+    drive_method_received = this.r2d2.drive_method
     while (true){
-        if drive_method !=== drive_method_received:
-            try unsubscribe
-            subscribe to drive_method_received
-            
+        if (current_drive_method !== drive_method_received){
+            if (current_drive_method){
+                unsubscribeDriveMethod(current_drive_method)
+            }
+            subscribeDriveMethod(drive_method_received)
+        }      
     }
 }
 
--- utilities/mqtt_connection.js
+++ utilities/mqtt_connection.js
@@ -1,4 +1,5 @@
 const mqtt = require('mqtt')
+const {handleManualControlRequest} = require('../r2d2_functions/move_functions')
 
 class MQTTConnection{
     constructor(ip_address, r2d2_initialized){
@@ -6,77 +7,24 @@ class MQTTConnection{
         this.client = mqtt.connect('mqtt://' + ip_address)
         console.log('Connected!')
         this.r2d2_initialized = r2d2_initialized
-        // todo: refactor
-        // this.client.on('connect', () => {
-        //     this.client.subscribe('rpi/manualControl')
-        //   })
-    
+        this.drive_method = null
+
+        this.drive_method_message_dict
+
+        this.client.on('connect', () => {
+            this.client.subscribe('rpi/chooseDriveMethod')
+          })
+        
         this.client.on('message', (topic, message) => {
         console.log('received message %s %s', topic, message)
         switch (topic) {
             case 'rpi/manualControl':
                 return handleManualControlRequest(message, this.r2d2_initialized)
+            case 'rpi/chooseDriveMethod':
+                this.drive_method = message
         }
         })
 }
 }
 
 exports.MQTTConnection = MQTTConnection
-
-async function handleManualControlRequest(message, r2d2_initialized) {
-    let manualRequest = parseManualControlRequest(message)
-    let speed = convertVelocityCompToMag(manualRequest.velX, manualRequest.velY)
-    let heading = convertJoystickRadtoHeading(manualRequest.ang)
-    console.log(speed + ' ' + heading)
-    await r2d2_initialized.manualRoll(speed, heading, [2])
-}
-
-/**
- * Calculates velocity magnitude from received x and y components.
- * Rounds output to second decimal.
- * @param {float} xvel : x component of velocity
- * @param {float} yvel : y component of velocity
- */
-function convertVelocityCompToMag(xvel, yvel){
-    vmag = Math.sqrt(Math.pow(xvel, 2) + Math.pow(yvel, 2))
-    if (vmag === 0){
-        return 0
-    }
-    else {
-        return (vmag/Math.SQRT2*100 + 150).toString(2)
-    }
-    
-}
-
-/**
- * Converts joystick angle in radians to heading in degrees CW.
- * 0 rad -> 0 deg
- * -1.57 rad -> 90 deg
- * 3.14 rad -> 180 deg
- * 1.57 rad -> 270 deg
- * @param {float} angle : angle of joystick in radians.
- */
-function convertJoystickRadtoHeading(angle){
-    if (angle <= 0) {
-        return (angle*-180/3.14).toString(2)
-    }
-    else {
-        return (360-angle*180/3.14).toString(2)
-    }
-}
-
-/**
- * Parses manual control message from app into velocity X/Y and angular components
- * @param {string} message : ... velX = [float] velY = [float] ang = [float]
- */
-function parseManualControlRequest(message){
-    messageString = message.toString()
-    let velX = messageString.split("velX = ")[1].split(" ")[0]
-    let velY = messageString.split("velY = ")[1].split(" ")[0]
-    let ang = messageString.split("ang = ")[1]
-    return{
-        velX: velX,
-        velY: velY,
-        ang: ang,
-    }
-}
