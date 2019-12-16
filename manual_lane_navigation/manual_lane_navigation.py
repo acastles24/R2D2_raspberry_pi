8831ce37add5433dac06e76368d64d46e7173dfc
Jman24 <adam.castles@gmail.com>
2019-12-15 20:59:00 -0500

    Started lane averaging

--- manual_lane_navigation/manual_lane_navigation.py
+++ manual_lane_navigation/manual_lane_navigation.py
@@ -1,5 +1,5 @@
 import cv2
-from utilities import BGR_to_HSV, blue_mask, detect_edges, crop_frame
+from utilities import BGR_to_HSV, blue_mask, detect_edges, crop_frame, line_segments
 
 frame_raw = cv2.imread('/home/pi/test_image.jpg')
 
@@ -10,6 +10,10 @@ frame_blue_mask = blue_mask(frame_hsv)
 frame_edges = detect_edges(frame_blue_mask)
 
 edges_cropped = crop_frame(frame_edges, 50, 50)
+
+lines_detected = line_segments(edges_cropped)
+
+print(lines_detected)
 
 cv2.imshow('blue_mask', edges_cropped)
 
--- manual_lane_navigation/utilities.py
+++ manual_lane_navigation/utilities.py
@@ -26,3 +26,45 @@ def crop_frame(frame, pct_height, pct_width):
 
     cv2.fillPoly(mask, polygon, 255)
     return cv2.bitwise_and(frame, mask)
+
+def line_segments(edges):
+    rho = 1
+    angle = np.pi/180
+    votes = 10
+    min_line_length = 8
+    max_gap = 4
+    return cv2.HoughLinesP(edges, rho, angle, votes, minLineLength=min_line_length, maxLineGap=max_gap)
+
+
+def average_lines(frame, line_segments):
+    lanes = []
+    if not line_segments:
+        print('No lanes detected')
+        return lanes
+    
+    height, width = frame.shape
+    left_lanes = []
+    right_lanes = []
+
+    #todo: check from camera images
+    lane_detection_boundary = 1/3
+
+    left_boundary = width * (1-lane_detection_boundary)
+    right_boundary = width * lane_detection_boundary
+
+    for segment in line_segments:
+        for x1, y1, x2, y2 in line_segments:
+            # disregard vertical line segments
+            if x1 == x2:
+                continue
+            fit = np.polyfit((x1, y1), (x2, y2))
+            slope = fit[0]
+            intercept = fit[1]
+            if slope < 0:
+                if x1 < left_boundary and x2 < left_boundary:
+                    left_lanes.append((slope, intercept))
+                else:
+                    if x1 > right_boundary and x2 > right_boundary:
+                        right_lanes.append((slope, intercept))
+    left_lane_average = np.average(left_lanes, axis=0)
+    
