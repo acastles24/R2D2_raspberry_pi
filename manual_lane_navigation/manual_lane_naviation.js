46c23153b1f75288af1874d1e6eeea5ecea63b2b
Jman24 <adam.castles@gmail.com>
2019-12-27 17:46:53 -0500

    Opencv camera set up

--- manual_lane_navigation/manual_lane_naviation.js
+++ manual_lane_navigation/manual_lane_naviation.js
@@ -6,10 +6,14 @@ class ManualLaneNav{
         this.camera = new cv.VideoCapture(0)
         this.camera.set(3, 320)
         this.camera.set(4, 240)
+        let image_lane = this.camera.read()
+        console.log(image_lane)
+        cv.imwrite('/home/pi/R2D2_raspberry_pi/test_images/test.jpg', image_lane)
     }
 
     async execute(message){
-        
+        _, image_lane = this.camera.read()
+        cv.imshow(image_lane)
     }
 }
 
