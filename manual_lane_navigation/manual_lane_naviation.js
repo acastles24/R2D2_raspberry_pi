818ded8cc56cddaba75940953dfed514f7715e43
Jman24 <adam.castles@gmail.com>
2019-12-29 14:28:49 -0500

    Added run python function

--- manual_lane_navigation/manual_lane_naviation.js
+++ manual_lane_navigation/manual_lane_naviation.js
@@ -1,4 +1,6 @@
 const cv = require('opencv4nodejs');
+const spawn = require('child_process').spawn
+
 
 class ManualLaneNav{
     constructor(r2d2_functions){
@@ -7,13 +9,44 @@ class ManualLaneNav{
         this.camera.set(3, 320)
         this.camera.set(4, 240)
         let image_lane = this.camera.read()
-        console.log(image_lane)
-        cv.imwrite('/home/pi/R2D2_raspberry_pi/test_images/test.jpg', image_lane)
+        run_python('./manual_lane_navigation/manual_lane_navigation.py')
+        // cv.imwrite('/home/pi/R2D2_raspberry_pi/test_images/test.jpg', image_lane)
     }
 
     async execute(message){
-        _, image_lane = this.camera.read()
-        cv.imshow(image_lane)
+        let image_lane = this.camera.read()
+        let python_process = spawn('python', ['manual_lane_navigation.py', image_lane])
+        python_process.stdout.on('data', (data) => {
+            console.log(data.toString())
+        })
+    }
+}
+
+
+function run_python(script_name, arg){
+    return new Promise((resolve, reject) => {
+        const process = spawn('python', [script_name, arg])
+        const out = []
+        process.stdout.on(
+            'data',
+            (data) => {
+                out.push(data.toString())
+                console.log(data.toString())
+            }
+        );
+        
+        const err = []
+        process.stderr.on(
+            'data',
+            (data) => {
+                err.push(data.toString())
+                console.log(data.toString() + ' Error')
+            }
+        );
+
+        process.on('exit', (code, signal)=>{
+            resolve(out)
+        }
     }
 }
 
--- manual_lane_navigation/manual_lane_navigation.py
+++ manual_lane_navigation/manual_lane_navigation.py
@@ -21,7 +21,7 @@ def detect_lanes(frame_raw):
 
 # frame_raw = cv2.imread('/home/pi/test_image.jpg')
 
-lanes, lane_image = detect_lanes(sys.argv[1])
+lanes, lane_image = detect_lanes(sys.argv)
 
 raw_angle = calc_steering_angle(frame_raw, lanes)
 
@@ -32,4 +32,5 @@ lanes_heading_image = display_heading(lane_image, raw_angle)
 # cv2.waitKey(0) # waits until a key is pressed
 # cv2.destroyAllWindows() # destroys the window showing image
 
-return raw_angle, lanes_heading_image
+print(lanes_heading_image, raw_angle)
+sys.stdout.flush()
