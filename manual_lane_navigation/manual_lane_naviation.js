a67b090f900ab1f3fc434e30823416ef9a14f8d4
Jman24 <adam.castles@gmail.com>
2019-12-29 14:40:43 -0500

    Python script successfully launches

--- manual_lane_navigation/manual_lane_naviation.js
+++ manual_lane_navigation/manual_lane_naviation.js
@@ -8,24 +8,21 @@ class ManualLaneNav{
         this.camera = new cv.VideoCapture(0)
         this.camera.set(3, 320)
         this.camera.set(4, 240)
-        let image_lane = this.camera.read()
-        run_python('./manual_lane_navigation/manual_lane_navigation.py')
+        this.execute('hi')
         // cv.imwrite('/home/pi/R2D2_raspberry_pi/test_images/test.jpg', image_lane)
     }
 
     async execute(message){
         let image_lane = this.camera.read()
-        let python_process = spawn('python', ['manual_lane_navigation.py', image_lane])
-        python_process.stdout.on('data', (data) => {
-            console.log(data.toString())
-        })
+        const steering_angle = await run_python('./manual_lane_navigation/manual_lane_navigation.py', image_lane)
+        console.log(steering_angle)
     }
 }
 
 
 function run_python(script_name, arg){
     return new Promise((resolve, reject) => {
-        const process = spawn('python', [script_name, arg])
+        const process = spawn('python3', [script_name, arg])
         const out = []
         process.stdout.on(
             'data',
@@ -45,9 +42,14 @@ function run_python(script_name, arg){
         );
 
         process.on('exit', (code, signal)=>{
-            resolve(out)
-        }
-    }
+            if (code === 0) {
+                resolve(out)
+            }
+            else{
+                reject(new Error(err.join('\n')))
+            }
+        })
+    })
 }
 
 exports.ManualLaneNav = ManualLaneNav
